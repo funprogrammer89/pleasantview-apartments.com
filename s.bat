f0456dd63a68438bd2f0f762005a976e04798eb3
Ken Elliott <Kennethpelliott@outlook.com>
2025-12-16 01:54:45 -0500

    added .batch file to call gitFTP script

---
+++ s.bat
@@ -0,0 +1,4 @@
+@echo off
+REM Use the call command to execute the script and return to the current script
+REM The path must be absolute (from the root drive, C:)
+call "C:\Users\whybo\Desktop\gitFTP.py"
