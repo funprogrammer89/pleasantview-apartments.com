80650b32a3fec2b3bddfa9666a679efce9e4718a
Ken Elliott <Kennethpelliott@outlook.com>
2025-12-17 01:26:05 -0500

    made a seperate PHP file for DB connections for security.......

--- blog/view.php
+++ blog/view.php
@@ -7,13 +7,6 @@ $Parsedown = new Parsedown();
 
 // 1. Pull in the database credentials
 require_once 'db.php';
- 
-$dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
-$options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
-];
 
 // 2. The connection logic stays here
 try {
--- blog/write.php
+++ blog/write.php
@@ -3,13 +3,6 @@
 // 1. Pull in the database credentials
 require_once 'db.php'; 
 
-$dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
-$options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
-];
-
 try {
      $pdo = new PDO($dsn, $user, $pass, $options);
 } catch (\PDOException $e) {
